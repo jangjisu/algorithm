99a3d425f2310a2a387002d2948a55ae0dc0c6a4
jangjisu <wltn85@naver.com>
2024-05-29 20:52:17 +0900

    디펜스 게임 프로그래머스 풀이 추가

---
+++ src/main/java/math/programmersDefenseGame.java
@@ -0,0 +1,39 @@
+package math;
+
+import java.util.PriorityQueue;
+
+public class programmersDefenseGame {
+    public static void main(String[] args) {
+        int[] enemy = {1,1,1,1,1,1,1};
+
+        System.out.println(solution(7, 3, enemy));
+    }
+
+    public static int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+
+        if(enemy.length <= k) {
+            return enemy.length;
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i=0; i<enemy.length; i++) {
+
+            pq.add(enemy[i]);
+
+            if(i >= k) {
+                int poll = pq.poll();
+
+                if(n - poll < 0){
+                    return i;
+                }else{
+                    n -= poll;
+                }
+            }
+        }
+
+
+        return enemy.length;
+    }
+}
